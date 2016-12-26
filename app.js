8766c644b7f86c8df34e0e180cddb3aa5e4ca8c7
Eric from BigD <exz1199@gmail.com>
2016-12-26 18:03:38 +0000

    moved script tags to top and deleted a var

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-$("#celsius").hide();
-$("#cBtn").hide();
 
-$("#fBtn").click(function() {
-  $("#fahrenheit").hide();
-  $("#fBtn").hide();
-  $("#celsius").show();
-  $("#cBtn").show();
-})
-
-$("#cBtn").click(function() {
+$(document).ready(function() {
   $("#celsius").hide();
   $("#cBtn").hide();
-  $("#fahrenheit").show();
-  $("#fBtn").show();
-})
-
-function located(position) {
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-  var celsius;
-  jQuery(document).ready(function($) {
-    $.ajax({
-      url: "https://api.wunderground.com/api/87e874167c96fe8e/conditions/q/" + lat + "," + long + ".json",
-      dataType: "jsonp",
-      success: function(parsed_json) {
-        $("#fahrenheit").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_f)));
-        
-        $("#celsius").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_c)));
-        $("#location").append(parsed_json.current_observation.display_location.full);
- $("#condition").append(parsed_json.current_observation.weather);
-      }
-    });
+  
+  $("#fBtn").click(function() {
+    $("#fahrenheit").hide();
+    $("#fBtn").hide();
+    $("#celsius").show();
+    $("#cBtn").show();
   });
-}
-
-navigator.geolocation.getCurrentPosition(located);
+  
+  $("#cBtn").click(function() {
+    $("#celsius").hide();
+    $("#cBtn").hide();
+    $("#fahrenheit").show();
+    $("#fBtn").show();
+  });
+  
+  function located(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    jQuery(document).ready(function($) {
+      $.ajax({
+        url: "https://api.wunderground.com/api/87e874167c96fe8e/conditions/q/" + lat + "," + long + ".json",
+        dataType: "jsonp",
+        success: function(parsed_json) {
+          $("#fahrenheit").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_f)));
+          
+          $("#celsius").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_c)));
+          $("#location").append(parsed_json.current_observation.display_location.full);
+   $("#condition").append(parsed_json.current_observation.weather);
+        }
+      });
+    });
+  }
+  
+  navigator.geolocation.getCurrentPosition(located);
+});
