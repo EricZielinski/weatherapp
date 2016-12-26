c7f7d27b9e6f844b4d5d720dd6c1807b5d7dfef0
Eric from BigD <exz1199@gmail.com>
2016-12-26 22:56:44 +0000

    uncommented js css and added link tag to index html

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 
-// $(document).ready(function() {
-//   $("#celsius").hide();
-//   $("#cBtn").hide();
+$(document).ready(function() {
+  $("#celsius").hide();
+  $("#cBtn").hide();
   
-//   $("#fBtn").click(function() {
-//     $("#fahrenheit").hide();
-//     $("#fBtn").hide();
-//     $("#celsius").show();
-//     $("#cBtn").show();
-//   });
+  $("#fBtn").click(function() {
+    $("#fahrenheit").hide();
+    $("#fBtn").hide();
+    $("#celsius").show();
+    $("#cBtn").show();
+  });
   
-//   $("#cBtn").click(function() {
-//     $("#celsius").hide();
-//     $("#cBtn").hide();
-//     $("#fahrenheit").show();
-//     $("#fBtn").show();
-//   });
+  $("#cBtn").click(function() {
+    $("#celsius").hide();
+    $("#cBtn").hide();
+    $("#fahrenheit").show();
+    $("#fBtn").show();
+  });
   
-//   function located(position) {
-//     var lat = position.coords.latitude;
-//     var long = position.coords.longitude;
-//     jQuery(document).ready(function($) {
-//       $.ajax({
-//         url: "https://api.wunderground.com/api/87e874167c96fe8e/conditions/q/" + lat + "," + long + ".json",
-//         dataType: "jsonp",
-//         success: function(parsed_json) {
-//           $("#fahrenheit").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_f)));
+  function located(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    jQuery(document).ready(function($) {
+      $.ajax({
+        url: "https://api.wunderground.com/api/87e874167c96fe8e/conditions/q/" + lat + "," + long + ".json",
+        dataType: "jsonp",
+        success: function(parsed_json) {
+          $("#fahrenheit").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_f)));
           
-//           $("#celsius").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_c)));
-//           $("#location").append(parsed_json.current_observation.display_location.full);
-//   $("#condition").append(parsed_json.current_observation.weather);
-//         }
-//       });
-//     });
-//   }
+          $("#celsius").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_c)));
+          $("#location").append(parsed_json.current_observation.display_location.full);
+  $("#condition").append(parsed_json.current_observation.weather);
+        }
+      });
+    });
+  }
   
-//   navigator.geolocation.getCurrentPosition(located);
-// });
+  navigator.geolocation.getCurrentPosition(located);
+});
