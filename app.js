34cedddb98714c3dd1d0529d6a8bb6dbefa19435
Eric from BigD <exz1199@gmail.com>
2016-12-26 23:47:05 +0000

    refactored app.js

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
 
 $(document).ready(function() {
-  $("#celsius").hide();
-  $("#cBtn").hide();
-  
-  $("#fBtn").click(function() {
-    $("#fahrenheit").hide();
-    $("#fBtn").hide();
-    $("#celsius").show();
-    $("#cBtn").show();
-  });
-  
-  $("#cBtn").click(function() {
-    $("#celsius").hide();
-    $("#cBtn").hide();
-    $("#fahrenheit").show();
-    $("#fBtn").show();
-  });
-  
-  function located(position) {
-    var lat = position.coords.latitude;
-    var long = position.coords.longitude;
-    jQuery(document).ready(function($) {
-      $.ajax({
-        url: "https://api.wunderground.com/api/87e874167c96fe8e/conditions/q/" + lat + "," + long + ".json",
-        dataType: "jsonp",
-        success: function(parsed_json) {
-          $("#fahrenheit").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_f)));
-          
-          $("#celsius").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_c)));
-          $("#location").append(parsed_json.current_observation.display_location.full);
-  $("#condition").append(parsed_json.current_observation.weather);
+    var lat;
+    var long;
+    
+    var weather = {
+        gps: function(position) {
+            navigator.geolocation.getCurrentPosition(located);
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+        },
+        initHideC: function() {
+            $("#celsius").hide();
+            $("#cBtn").hide();
+        },
+        selectF: function() {
+            $("#fBtn").click(function() {
+                $("#fahrenheit").hide();
+                $("#fBtn").hide();
+                $("#celsius").show();
+                $("#cBtn").show();
+            })
+        },
+        selectC: function() {
+             $("#cBtn").click(function() {
+                $("#celsius").hide();
+                $("#cBtn").hide();
+                $("#fahrenheit").show();
+                $("#fBtn").show();
+            });
+        },
+        xhr: function() {
+            $.ajax({
+                url: "https://api.wunderground.com/api/87e874167c96fe8e/conditions/q/" + lat + "," + long + ".json",
+                dataType: "jsonp",
+                success: function(parsed_json) {
+                  $("#fahrenheit").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_f)));
+                  $("#celsius").prepend(document.createTextNode(Math.round(parsed_json.current_observation.temp_c)));
+                  $("#location").append(parsed_json.current_observation.display_location.full);
+                  $("#condition").append(parsed_json.current_observation.weather);
+                }
+            });
         }
-      });
-    });
-  }
-  
-  navigator.geolocation.getCurrentPosition(located);
+    }
+    
+    weather.initHideC();
+    weather.selectC();
+    weather.selectF();
 });
